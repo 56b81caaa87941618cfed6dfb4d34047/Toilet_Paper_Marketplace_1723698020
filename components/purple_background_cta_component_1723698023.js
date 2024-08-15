56ea6ac5f9d142e7256300734afcb27eea11af46
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-15 05:19:18 +0000

    Code generation

--- components/purple_background_cta_component_1723698023.js
+++ components/purple_background_cta_component_1723698023.js
@@ -34,18 +34,18 @@ Vue.component("purple_background_cta_component_1723698023", {
             return {
                 expanded: false, 
                 tab: null,
-                radialGradientPrimaryStyle: "bg-purple-500",
-                radialGradientSecondaryStyle: "bg-purple-400",
-                stopColorPrimaryStyle: "#A855F7",
-                stopColorSecondaryStyle: "#6366F1",
+                radialGradientPrimaryStyle: "bg-yellow-300",
+                radialGradientSecondaryStyle: "bg-yellow-200",
+                stopColorPrimaryStyle: "#FDE047",
+                stopColorSecondaryStyle: "#FEF08A",
                 transparentTextStyle: "text-transparent",
-                contentSubtitleStyle: "bg-gradient-to-r from-purple-500 to-purple-200",
+                contentSubtitleStyle: "bg-gradient-to-r from-yellow-400 to-yellow-200",
                 contentCtaTitleStyle: "bg-gradient-to-r from-slate-200/60 via-slate-200 to-slate-200/60",
                 contentBodyStyle: "text-slate-400",
                 contentCtaButtonPrimaryStyle:"text-slate-900",
                 contentCtaButtonGradientStyle: "bg-gradient-to-r from-white/80 via-white to-white/80",
                 contentCtaButtonHoverStyle: "hover:bg-white",
-                contentCtaArrowStyle: "text-purple-500"
+                contentCtaArrowStyle: "text-yellow-500"
             };
         },
     });
